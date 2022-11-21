cdc1c239c56ae308b742874471bf0bc5e2a5756d
JiwonLuna <jwluna1301@gmail.com>
2022-11-22 02:49:31 +0900

    Feat: Add fuction to show blocks

--- substitute.cpp
+++ substitute.cpp
@@ -4,20 +4,38 @@
 #define height 22
 
 class Tetris {
+    const char* full = "бс";
+    const char* empty = "бр";
 
     public:
     int tet[height][width] = { 0 };
-    void show_stat();
+    void show_stat_num();
+    void show_stat_sym();
     void set_edge();
-
+    void gen_block();
 
 };
 
-void Tetris::show_stat() {
+void Tetris::show_stat_num() {
     for (int i = 0; i < height; i++) {
         for (int j = 0; j < width; j++) {
             std::cout << tet[i][j];
         }
+        std::cout << std::endl;
+    }
+}
+
+void Tetris::show_stat_sym() {
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            if(tet[i][j]==1){
+                std::cout << full;
+            }
+            else if(tet[i][j]==0) {
+                std::cout << empty;
+            }
+        }
+        
         std::cout << std::endl;
     }
 }
@@ -33,10 +51,15 @@ void Tetris::set_edge() {
     }
 }
 
+void Tetris::gen_block() {
+    
+}
+
 int main() {
     Tetris tet;
     tet.set_edge();
-    tet.show_stat();
+    tet.show_stat_num();
+    tet.show_stat_sym();
 
 
     return 0;
